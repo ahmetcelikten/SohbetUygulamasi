53f64ab1414f0d2ff4d0f36822088d0f46e4f69a
ahmetcelikten <ahmetcelikten@hotmail.com.tr>
2024-12-08 23:37:56 +0300

    Proje dosyası ekle.

---
+++ ChatApp/MVM/View/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ChatClient.MVM.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
